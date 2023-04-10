406dd0685b9238d6085e4d2684ba0896dbb1ea08
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-04-10 10:59:19 +0200

    Update 2 numbers code.py

--- 2 numbers code.py
+++ 2 numbers code.py
@@ -1,83 +1,26 @@
 import random
 
-list = ["Rock", "Scissors", "Paper"]
-
+options = ["Rock", "Scissors", "Paper"]
 
 def play():
-    global a, b, Want_To_Play
-    Want_To_Play = True
-    while Want_To_Play == True:
-        a = random.randint(0, 2)
-        b = input("What do you choose rock, paper or scissors?: ")
-        a = list[a]
-        l = f"Unfortunately you've lost the game your opponent had a {a}."
-        w = f"Congrats you've won the game your opponent had a {a}."
-        d = f"It is a draw, both of you had a {a}."
-        if b == "Rock" and a == "Rock":
-            print(d)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Paper" and a == "Paper":
-            print(d)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Scissors" and a == "Scissors":
-            print(d)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Rock" and a == "Paper":
-            print(l)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Rock" and a == "Scissors":
-            print(w)
-            p = input("Do you want to play a game once more:")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Paper" and a == "Scissors":
-            print(l)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Paper" and a == "Rock":
-            print(w)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Scissors" and a == "Rock":
-            print(l)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
-        elif b == "Scissors" and a == "Paper":
-            print(w)
-            p = input("Do you want to play a game once more: ")
-            if p == "Yes":
-                Want_To_Play = True
-            elif p == "No":
-                Want_To_Play = False
+    while True:
+        computer_choice = random.choice(options)
+        your_choice = input("What do you pick: Rock, Paper or Scissors? ")
+        if your_choice not in options:
+            print("Please play by rules and choose a real option.")
+        elif your_choice == computer_choice:
+            print(f"That's a draw. Your opponent's pick was {computer_choice}.")
+        elif (your_choice == "Scissors" and computer_choice == "Paper" ) or (your_choice == "Paper" and computer_choice == "Rock") or (your_choice == "Rock" and computer_choice == "Scissors"):
+            print(f"You've won the game, your opponent's pick was {computer_choice}.")
+        else:
+            print(f"You've lost the game, your opponent's pick was {computer_choice}.")
+        a = input("Do you want to continue playing the game? (Yes/No) : ")
+        if a.upper() == "NO":
+            break
+
+
+
 
 play()
-
 
 
